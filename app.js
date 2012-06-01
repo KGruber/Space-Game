58e7c3e78b298a47bafeafb2ea9ec35adb4af09c
KGruber <Hans.Andersson@1337pages.com>
2012-06-01 11:37:14 -0700

    Removed ControllerBootstrapper call from app.js

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ var express = require('express'),
 var path = __dirname;
 
 var boot = function(){
-	var app = express.createServer();	
+    var app = express.createServer();	
     
-	importConfigurationSettings(app);
-	initializeMiddleware(app);
+    importConfigurationSettings(app);
+    initializeMiddleware(app);
     initializeViewEngine(app);
     addErrorHandling(app);
     apiRequestBootstrapper.boot(app);
-	controllerBootstrapper.boot(app);
     startListening(app);
 };
 
@@ -24,17 +23,17 @@ var importConfigurationSettings = function(app){
 var initializeViewEngine = function (app){
     app.set('views', path + '/views');
     app.register('.coffee', require('coffeekup').adapters.express);
-	app.set('view engine', 'coffee');
+    app.set('view engine', 'coffee');
 };
 
 var initializeMiddleware = function (app) {
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: app.set('sessionKey') }));
-	app.use(express.static(path + '/public'));  // Before router to enable dynamic routing
-	app.use(authenticationRequestWrapper); // Before route to force authentication before controllers act
-	app.use(app.router);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: app.set('sessionKey') }));
+    app.use(express.static(path + '/public'));  // Before router to enable dynamic routing
+    app.use(authenticationRequestWrapper); // Before route to force authentication before controllers act
+    app.use(app.router);
 };
 
 var addErrorHandling = function (app){
